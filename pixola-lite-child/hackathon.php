2f98dd5d1d65a6dbf566256dd5ceb714ff999596
Boyquotes <webmaster@linux-live-cd.org>
2016-03-09 13:00:42 +0100

    Theme Pixola et son thème enfant + modèle de page

---
+++ pixola-lite-child/hackathon.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Template Name: Hackathon Simplon Sud
+*/
+?>
+<?php
+while ( have_posts() ) : the_post();
+  the_content();
+endwhile; // end of the loop.
+?>
---
+++ pixola-lite-child/page.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+<?php get_template_part('sections/section', 'header-page'); ?>
+<div class="has-padding">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<?php while ( have_posts() ) : the_post(); ?>
+                <div class="container">
+                    <div class="row">
+                            <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                                <div class="entry-content">
+																	<h2>Le contenu</h2>
+                                    <?php
+                                    the_content();
+
+                                    wp_link_pages( array(
+                                        'before' => '<nav class="page-links">' . __( 'Pages:', 'pixova-lite' ),
+                                        'after'  => '</nav>',
+                                    ) );
+                                    ?>
+                                </div><!-- .entry-content -->
+                            </div><!-- #post-## -->
+                        <?php comments_template(); ?>
+                    </div><!--/.row-->
+                </div><!--/.container-->
+			<?php endwhile; // end of the loop. ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div>
+<?php get_footer(); ?>
